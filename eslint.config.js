fee3ac20dace001eec1c872ce1fe3c20df02c108
nikaginger <brenevaveronika@gmail.com>
2025-11-22 13:43:56 +0400

    chore: FSD structure + aliases added

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,28 +13,27 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-        ...tseslint.configs.recommended,
-        pluginReact.configs.recommended,
-        pluginReactHooks.configs.recommended,
-        eslintPluginPrettier,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      eslintPluginPrettier,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-        {
-            rules: {
-                "prettier/prettier": "error",
-                "@typescript-eslint/consistent-type-imports": "error",
-                "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
-            }
-        },
-        {
-            settings: {
-                react: { version: "detect" }
-            }
-        }
+      reactRefresh.configs.vite
     ],
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' }
+      ],
+      'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+      react: { version: 'detect' }
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+      globals: globals.browser
+    }
+  }
+]);
