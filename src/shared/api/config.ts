293041ddae5a95ca4efc074e184031772fcd389e
nikaginger <brenevaveronika@gmail.com>
2025-11-23 23:43:31 +0400

    fix: proxy added for server calls

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:3001/api/v1';
+export const API_BASE_URL = '/api/v1';
 
 export const http = axios.create({
   baseURL: API_BASE_URL
